0f21a643be4a2ce530fa172be9d9ec04469781d9
PiingPiing <artittaya.mahis@gmail.com>
2020-03-06 21:19:36 +0700

    first commit

---
+++ src/test/java/me/artittaya/NumericaMethod/NumericaMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package me.artittaya.NumericaMethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumericaMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
